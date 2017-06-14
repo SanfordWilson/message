fdc6911770b4632b2217ccbd80f96d40f007d449
Sanford Wilson <sanford.wilson@gmail.com>
2017-06-14 12:20:35 -0400

    Initial Commit

---
+++ Dummy Messager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dummy Messager
+//
+//  Created by Sanford Wilson on 6/14/17.
+//  Copyright © 2017 Sanford Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
