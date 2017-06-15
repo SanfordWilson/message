55675573dfe4f49bc10144ef10357e283681b38a
Sanford Wilson <sanford.wilson@gmail.com>
2017-06-14 21:16:48 -0400

    Added prototype cell displays, and Chat class file \

--- Dummy Messager/ChatsTableViewController.swift
+++ Dummy Messager/ChatsTableViewController.swift
@@ -12,12 +12,29 @@ import UIKit
 class ChatsTableViewController: UITableViewController {
     
     let cellReuseIdentifier = "ChatCell"
+    var sampleData = [String]()
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
  
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return sampleData.count
     }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        if let lable = cell.textLabel {
+            lable.text = sampleData[indexPath.row]
+        }
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 0..<100 {
+            sampleData.append("Cell \(i)")
+        }
+    }
+    
 }
