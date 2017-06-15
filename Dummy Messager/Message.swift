a18816a53ed18bf7142f57ffecd8152489461a35
Sanford Wilson <sanford.wilson@gmail.com>
2017-06-14 23:14:31 -0400

    Added empty Message class, and implemented some placeholder code for Chat

---
+++ Dummy Messager/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Dummy Messager
+//
+//  Created by Sanford Wilson on 6/14/17.
+//  Copyright © 2017 Sanford Wilson. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+}
