c9c1a907384df66032b616d7b225aa0effaa678d
Sanford Wilson <sanford.wilson@gmail.com>
2017-06-17 13:19:48 -0400

    Removed storyboard, converted ChatsTableViewController to a UICollectionView with background color as a placeholder

---
+++ Dummy Messager/ChatsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Dummy Messager
+//
+//  Created by Sanford Wilson on 6/14/17.
+//  Copyright © 2017 Sanford Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ChatsViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+    }
+}
