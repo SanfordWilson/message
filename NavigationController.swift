a851dbd0fa21441ed822367e0bde02506447c91d
Sanford Wilson <sanford.wilson@gmail.com>
2017-07-25 14:10:37 -0400

    Added image assets for false status bar, tweaked status bar

--- NavigationController.swift
+++ NavigationController.swift
@@ -16,10 +16,11 @@ class NavigationController: UINavigationController {
         return bar
     }()
     
-    let connectionStrengthButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.black
-        return button
+    let connectionStrengthImage: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: "ConnectionLevel4")
+        return view
     }()
     
     let carrierLabel: UILabel = {
@@ -32,7 +33,7 @@ class NavigationController: UINavigationController {
     let signalTypeLabel: UILabel = {
         let label = UILabel()
         label.text = "LTE"
-        label.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+        label.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize, weight: UIFontWeightThin)
         return label
     }()
     
@@ -49,14 +50,22 @@ class NavigationController: UINavigationController {
         formatter.dateStyle = .none
         formatter.timeStyle = .short
         label.text = formatter.string(from: currentTime)
-        label.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+        label.font = UIFont.boldSystemFont(ofSize: UIFont.smallSystemFontSize)
         return label
     }()
     
-    let batteryImageButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.black
-        return button
+    let batteryImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: "BatteryFull")
+        return view
+    }()
+    
+    let chargingImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.image = UIImage(named: "ChargingIndicator")
+        return view
     }()
     
     override func viewDidLoad() {
@@ -68,20 +77,23 @@ class NavigationController: UINavigationController {
         view.addSubview(statusBar)
         view.visuallyFormat(format: "H:|[v0]|", views: statusBar)
         view.visuallyFormat(format: "V:|[v0(20)]", views: statusBar)
-        statusBar.addSubview(connectionStrengthButton)
+        statusBar.addSubview(connectionStrengthImage)
         statusBar.addSubview(carrierLabel)
         statusBar.addSubview(signalTypeLabel)
         statusBar.addSubview(wifiStrengthButton)
         statusBar.addSubview(dateLabel)
-        statusBar.addSubview(batteryImageButton)
-        statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: connectionStrengthButton)
+        statusBar.addSubview(batteryImageView)
+        statusBar.addSubview(chargingImageView)
+        statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: connectionStrengthImage)
         statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: carrierLabel)
         statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: signalTypeLabel)
         statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: wifiStrengthButton)
         statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: dateLabel)
-        statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: batteryImageButton)
-        statusBar.visuallyFormat(format: "H:|-5-[v0(40)]-2-[v1]-2-[v2]-2-[v3(20)]", views: connectionStrengthButton, carrierLabel, signalTypeLabel, wifiStrengthButton)
+        statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: batteryImageView)
+        statusBar.visuallyFormat(format: "V:|-5-[v0]-5-|", views: chargingImageView)
+        statusBar.visuallyFormat(format: "H:|-5-[v0(40)]-2-[v1]-2-[v2]-2-[v3(20)]", views: connectionStrengthImage, carrierLabel, signalTypeLabel, wifiStrengthButton)
         statusBar.addConstraint(NSLayoutConstraint(item: dateLabel, attribute: .centerX, relatedBy: .equal, toItem: statusBar, attribute: .centerX, multiplier: 1.0, constant: 0.0))
-        statusBar.visuallyFormat(format: "H:[v0(25)]-5-|", views: batteryImageButton)
+        statusBar.visuallyFormat(format: "H:[v0(20)]-2-[v1(5)]-5-|", views: batteryImageView, chargingImageView)
+        wifiStrengthButton.isHidden = true
     }
 }
